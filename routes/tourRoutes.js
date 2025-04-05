71318bc13be56b1c452e8bf8c9979335b5e35e6b
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-05 17:11:26 -0300

    Chaining middleware handler to validate the req body

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,10 +4,15 @@ const tourController = require('../controllers/tourController');
 const router = express.Router();
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware function
+// Check if body contains the name property and price property
+// If not, send back a 400 (bad request)
+// Add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 router
   .route('/:id')
   .delete(tourController.deleteTour)
