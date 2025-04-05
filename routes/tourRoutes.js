dbd4dcafeb8c07512c540e75e5d826755e47edce
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-05 14:27:11 -0300

    Building router middleware - separate modules

---
+++ routes/tourRoutes.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+
+const express = require('express');
+
+const router = express.Router();
+
+const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour)
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+
+  res
+    .status(200)
+    .send(
+      `<h1>${tour.name}</h1><h2>${tour.summary}</h2><h3>${tour.description}</h3>`
+    );
+};
+
+const updateTour = (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated data here>',
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+const deleteTour = (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').delete(deleteTour).get(getTour).patch(updateTour);
+
+module.exports = router;
