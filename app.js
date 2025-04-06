1d1ef3de9d29840d7a9963871bbdc15fbd2c0c00
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-05 21:42:03 -0300

    Setting up env. vars. and addition dev packages

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARE
+console.log(process.env.NODE_ENV);
 
-app.use(morgan('dev'));
+// 1) MIDDLEWARE
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public/`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
