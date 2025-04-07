7c16d5d4e9c011f67915aaa67f309abd8f4ebb67
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-07 09:41:05 -0300

    Implementing basic CRUD operations

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,19 +30,30 @@ exports.getTour = async (req, res) => {
     });
   } catch (err) {
     res.status(404).json({
-      satus: 'fail',
+      status: 'fail',
       message: err,
     });
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated data here>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -65,9 +76,18 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
