5bee3ddf17d06b77f9024053abd72c8939e79591
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-06 22:20:10 -0300

    getTour handler and getAllTours from the DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,27 +1,39 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // const id = +req.params.id;
-  // const tour = tours.at(id);
-  // res.status(200).json({
-  //   message: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      message: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      satus: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
