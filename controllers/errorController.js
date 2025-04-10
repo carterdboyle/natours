6f7751337e55b64bd0dfe0084dbc1dcf3de521c3
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-10 11:52:51 -0300

    Setting up ValidationErrors from MongoDB and unhandled rejections

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,18 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: "${err.keyValue.name}". Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
@@ -44,6 +56,9 @@ module.exports = (err, req, res, next) => {
     let error = JSON.parse(JSON.stringify(err));
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
