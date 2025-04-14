5ae961a0f78c9c37459d8890eddec92a601e0648
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-14 17:56:11 -0300

    Helmet middleware added to app.js for more secure HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -12,10 +13,15 @@ const app = express();
 console.log(process.env.NODE_ENV);
 
 // 1) MIDDLEWARE
+// Set security HTTP Headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 3,
   windowMs: 60 * 60 * 1000,
@@ -23,9 +29,13 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public/`));
 
+// Test middlewares
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
