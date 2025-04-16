1a09674e24717e216375e0cb406660b855b1fbd6
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-04-16 10:58:55 -0300

    updating tour reviews when an update is added

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 // review / rating / createdAt / ref to tour / ref to user
 const reviewSchema = new mongoose.Schema(
@@ -34,6 +35,34 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   },
 );
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review - use constructor to get the model
+  // const Review = ... is below in the code so Review model doesn't
+  // exist yet
+  this.constructor.calcAverageRatings(this.tour);
+});
 
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
