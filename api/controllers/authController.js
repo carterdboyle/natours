85dda4c031a604da409c7a9e2777312deabfea4b
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-05-06 21:10:34 -0300

    Update authController.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -17,7 +17,6 @@ const createSendToken = (user, statusCode, req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
-    domain: 'localhost',
     httpOnly: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
     sameSite: 'lax',
